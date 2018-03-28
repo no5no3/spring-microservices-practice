9d882ea8c9ce84c28ab93c15cc2f9d656b14e153
hirenloong <hirenloong@gmail.com>
2018-03-29 07:42:22 +0800

    miniprogram login

--- account-service/src/main/java/net/no5no3/microservices/account/controller/MiniprogramController.java
+++ account-service/src/main/java/net/no5no3/microservices/account/controller/MiniprogramController.java
@@ -2,7 +2,7 @@ package net.no5no3.microservices.account.controller;
 
 import net.no5no3.microservices.account.model.User;
 import net.no5no3.microservices.account.service.AccountService;
-import org.apache.commons.lang.math.RandomUtils;
+import net.no5no3.microservices.account.util.HttpUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
@@ -22,27 +22,25 @@ public class MiniprogramController {
     private AccountService accountService;
     @Autowired
     private StringRedisTemplate template;
+
     @GetMapping(value = "/authorize/miniprogram")
     public Map authorize(@RequestParam String c) throws Exception {
-//        Map<String, Object> params = new HashMap<>();
-//        params.put("grant_type","authorization_code");
-//        params.put("appid","wx2f5b54e74a8f7809");
-//        params.put("secret","f635cccb938fc793cdada40b8a142291");
-//        params.put("js_code",c);
-//        Map res = HttpUtil.httpGet(API,params);
-//        Map result = new HashMap();
-//        if(res.containsKey("errcode")){//error
-//            result = res;
-//        }else{
-//            User user = accountService.miniprogramUser(res);
-        User user = new User();
-        user.setId(RandomUtils.nextInt());
-        Map res = new HashMap();
+        Map<String, Object> params = new HashMap<>();
+        params.put("grant_type", "authorization_code");
+        params.put("appid", "wx2f5b54e74a8f7809");
+        params.put("secret", "f635cccb938fc793cdada40b8a142291");
+        params.put("js_code", c);
+        Map res = HttpUtil.httpGet(API, params);
         Map result = new HashMap();
-        ValueOperations<String,String> operations = template.opsForValue();
-        String token = UUID.randomUUID().toString().replaceAll("[\\W]","").toUpperCase();
-        operations.set(token,String.format("%d-%s",user.getId(),res.get("session_key")),2, TimeUnit.HOURS);
-        result.put("token", token);
+        if (res.containsKey("errcode")) {//error
+            result = res;
+        } else {
+            User user = accountService.miniprogramUser(res);
+            ValueOperations<String, String> operations = template.opsForValue();
+            String token = UUID.randomUUID().toString().replaceAll("[\\W]", "").toUpperCase();
+            operations.set(token, String.format("%d-%s", user.getId(), res.get("session_key")), 2, TimeUnit.HOURS);
+            result.put("token", token);
+        }
         return result;
     }
 }
