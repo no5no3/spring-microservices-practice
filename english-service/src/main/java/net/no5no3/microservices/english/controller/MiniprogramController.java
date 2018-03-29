9be132a702674fe962f8d1777b1a8052a914624f
hirenloong <hirenloong@gmail.com>
2018-03-29 15:20:51 +0800

    token

--- english-service/src/main/java/net/no5no3/microservices/english/controller/MiniprogramController.java
+++ english-service/src/main/java/net/no5no3/microservices/english/controller/MiniprogramController.java
@@ -3,6 +3,7 @@ package net.no5no3.microservices.english.controller;
 import net.no5no3.microservices.english.model.User;
 import net.no5no3.microservices.english.service.AccountService;
 import net.no5no3.microservices.english.util.HttpUtil;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.HeaderParam;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -25,8 +27,13 @@ public class MiniprogramController {
     private StringRedisTemplate template;
 
     @GetMapping(value = "/authorize/miniprogram")
-    public Map authorize(@RequestParam String c, HttpServletRequest request) throws Exception {
+    public Map authorize(@RequestParam String c, HttpServletRequest request, @HeaderParam("token") String token) throws Exception {
         Map<String, Object> params = new HashMap<>();
+        ValueOperations<String, String> operations = template.opsForValue();
+        if(StringUtils.isNotEmpty(token) && operations.getOperations().getExpire(token) > 0){
+            params.put("token", token);
+            return params;
+        }//end
         params.put("grant_type", "authorization_code");
         params.put("appid", "wx2f5b54e74a8f7809");
         params.put("secret", "f635cccb938fc793cdada40b8a142291");
@@ -37,8 +44,7 @@ public class MiniprogramController {
             throw new RuntimeException(res.get("errcode").toString());
         } else {
             User user = accountService.miniprogramUser(res);
-            ValueOperations<String, String> operations = template.opsForValue();
-            String token = UUID.randomUUID().toString().replaceAll("[\\W]", "").toUpperCase();
+            token = UUID.randomUUID().toString().replaceAll("[\\W]", "").toUpperCase();
             operations.set(token, String.format("%d-%s", user.getId(), res.get("session_key")), 2, TimeUnit.HOURS);
             result.put("token", token);
         }
